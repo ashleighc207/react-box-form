9776484f985b342f0d2b8e2faa5ccd582be21bd1
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-20 12:11:19 -0400

    add in UUID package to generate random keys

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import ColorBoard from './ColorBoard.js';
-import BoxForm from './BoxForm.js';
 
 function App() {
   return (
     <div className="App">
-      <BoxForm />
       <ColorBoard />
     </div>
   );
--- src/ColorBoard.js
+++ src/ColorBoard.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import './ColorBoard.css';
-import ColorBox from './ColorBox.js'
+import ColorBox from './ColorBox.js';
+import BoxForm from './BoxForm.js';
+const uuid = require('uuid/v4');
+
 
 class ColorBoard extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      boxes: []
+    }
+  }
   render(){
     return(
-      <div>
-      Colorboard
+      <div className="ColorBoard">
+        <BoxForm />
         <ColorBox />
       </div>
-
     )
   }
 }
