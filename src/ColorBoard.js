7a909c8280702a828438a63d3ae23655b47e3a54
Ashleigh Christopher <ashleighc207@gmail.com>
2019-09-23 09:06:14 -0400

    remove unused instance of UUID

--- src/ColorBoard.js
+++ src/ColorBoard.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './ColorBoard.css';
 import ColorBox from './ColorBox.js';
 import BoxForm from './BoxForm.js';
-const uuid = require('uuid/v4');
 
 
 class ColorBoard extends Component {
