77bc7f72099712ca9cd7a0586e5584f0dd14afdd
ashleigh <ashleighc207@gmail.com>
2022-08-16 13:59:37 -0400

    update deps'

--- src/BoxForm.js
+++ src/BoxForm.js
@@ -1,56 +1,56 @@
-import React, { Component } from 'react';
-import './BoxForm.css';
-const uuid = require('uuid/v4');
+import React, { Component } from "react";
+import "./BoxForm.css";
+import { v4 as uuid } from "uuid";
 
 class BoxForm extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleFocus = this.handleFocus.bind(this);
     this.handleBlur = this.handleBlur.bind(this);
     this.state = {
-      width: '',
-      height: '',
-      color: ''
-    }
+      width: "",
+      height: "",
+      color: ""
+    };
   }
 
-  handleChange(evt){
+  handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value
-    })
+    });
   }
 
-  handleFocus(evt){
-    evt.target.previousSibling.classList.add('active')
+  handleFocus(evt) {
+    evt.target.previousSibling.classList.add("active");
   }
 
-  handleBlur(evt){
-    if(evt.target.value === "") {
-      evt.target.previousSibling.classList.remove('active')
+  handleBlur(evt) {
+    if (evt.target.value === "") {
+      evt.target.previousSibling.classList.remove("active");
     }
   }
 
-  handleSubmit(evt){
+  handleSubmit(evt) {
     evt.preventDefault();
-    const boxState = {...this.state, id: uuid()}
-    this.props.addNewBox(boxState)
+    const boxState = { ...this.state, id: uuid() };
+    this.props.addNewBox(boxState);
     this.setState({
-      width: '',
-      height: '',
-      color: ''
-    })
-    let labels = document.getElementsByTagName('label');
-    for(let i = 0; i < labels.length; i++){
-      labels[i].classList.remove('active')
+      width: "",
+      height: "",
+      color: ""
+    });
+    let labels = document.getElementsByTagName("label");
+    for (let i = 0; i < labels.length; i++) {
+      labels[i].classList.remove("active");
     }
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="BoxForm">
-      <h1 className="BoxForm--heading_one"> Create a new box! </h1>
+        <h1 className="BoxForm--heading_one"> Create a new box! </h1>
         <form onSubmit={this.handleSubmit}>
           <div className="BoxForm--input_group">
             <label htmlFor="width-input">Width</label>
@@ -76,7 +76,7 @@ class BoxForm extends Component {
               onBlur={this.handleBlur}
               value={this.state.height}
               required
-              />
+            />
           </div>
           <div className="BoxForm--input_group">
             <label htmlFor="color-input">Color</label>
@@ -90,12 +90,14 @@ class BoxForm extends Component {
               value={this.state.color}
               required
             />
-            <span className="BoxForm--input_helper_text">This field supports color hex, rgb/rgba, hsl, and named colors</span>
+            <span className="BoxForm--input_helper_text">
+              This field supports color hex, rgb/rgba, hsl, and named colors
+            </span>
           </div>
           <button className="BoxForm--submit_btn">Add box</button>
         </form>
       </div>
-    )
+    );
   }
 }
 
